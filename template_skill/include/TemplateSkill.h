64401f56b8db4719681006ff8cfe674979a0bab6
Stefano Bernagozzi <stefano.bernagozzi@iit.it>
2025-09-05 14:20:28 +0200

    updated template skill
    Signed-off-by: Stefano Bernagozzi <stefano.bernagozzi@iit.it>

--- template_skill/include/TemplateSkill.h
+++ template_skill/include/TemplateSkill.h
@@ -5,15 +5,15 @@
 #include <rclcpp/rclcpp.hpp>
 #include "rclcpp_action/rclcpp_action.hpp"
 #include "$className$SM.h"
-#include <bt_interfaces/msg/$skillTypeLC$_response.hpp>/*INTERFACES_LIST*/
+#include <bt_interfaces_dummy/msg/$skillTypeLC$_response.hpp>/*INTERFACES_LIST*/
 /*INTERFACE*/
 #include <$eventData.interfaceName$/srv/$eventData.functionNameSnakeCase$.hpp> /*END_INTERFACE*/
 /*ACTION_INTERFACE*/
 #include <$eventData.interfaceName$/action/$eventData.functionNameSnakeCase$.hpp> /*END_ACTION_INTERFACE*/
 /*TOPIC_INTERFACE*/
-#include <$eventData.interfaceData[interfaceDataType]$.hpp> /*END_TOPIC_INTERFACE*/
-/*TICK*/#include <bt_interfaces/srv/tick_$skillTypeLC$.hpp>/*END_TICK*/
-/*HALT*/#include <bt_interfaces/srv/halt_$skillTypeLC$.hpp>/*END_HALT*/
+#include <$eventData.interfaceName$/msg/$eventData.messageNameSnakeCase$.hpp> /*END_TOPIC_INTERFACE*/
+/*TICK*/#include <bt_interfaces_dummy/srv/tick_$skillTypeLC$.hpp>/*END_TICK*/
+/*HALT*/#include <bt_interfaces_dummy/srv/halt_$skillTypeLC$.hpp>/*END_HALT*/
 /*DATAMODEL*/
 #include "$skillName$SkillDataModel.h" /*END_DATAMODEL*/
 
@@ -36,13 +36,13 @@ public:
 	bool start(int argc, char * argv[]);
 	static void spin(std::shared_ptr<rclcpp::Node> node);
 	/*TICK_CMD*/
-	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::Tick$skillType$::Request> request,
-			   std::shared_ptr<bt_interfaces::srv::Tick$skillType$::Response>      response);/*END_TICK_CMD*/
+	void tick( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::Tick$skillType$::Request> request,
+			   std::shared_ptr<bt_interfaces_dummy::srv::Tick$skillType$::Response>      response);/*END_TICK_CMD*/
 	/*HALT_CMD*/
-	void halt( [[maybe_unused]] const std::shared_ptr<bt_interfaces::srv::HaltAction::Request> request,
-			   [[maybe_unused]] std::shared_ptr<bt_interfaces::srv::HaltAction::Response> response);/*END_HALT_CMD*/
+	void halt( [[maybe_unused]] const std::shared_ptr<bt_interfaces_dummy::srv::HaltAction::Request> request,
+			   [[maybe_unused]] std::shared_ptr<bt_interfaces_dummy::srv::HaltAction::Response> response);/*END_HALT_CMD*/
 	/*TOPIC_CALLBACK_LIST_H*/
-	/*TOPIC_CALLBACK_H*/void topic_callback_$eventData.functionName$(const $eventData.interfaceData[interfaceDataType]$::SharedPtr msg);/*END_TOPIC_CALLBACK_H*/
+	/*TOPIC_CALLBACK_H*/void topic_callback_$eventData.functionName$(const $eventData.interfaceName$::msg::$eventData.messageNameSnakeCase$::SharedPtr msg);/*END_TOPIC_CALLBACK_H*/
 
 private:
 	std::shared_ptr<std::thread> m_threadSpin;
@@ -51,13 +51,13 @@ private:
 	std::string m_name;
 	$SMName$ m_stateMachine;
 	/*TICK_RESPONSE*/std::atomic<Status> m_tickResult{Status::undefined};/*END_TICK_RESPONSE*/
-	/*TICK_CMD*/rclcpp::Service<bt_interfaces::srv::Tick$skillType$>::SharedPtr m_tickService;/*END_TICK_CMD*/
+	/*TICK_CMD*/rclcpp::Service<bt_interfaces_dummy::srv::Tick$skillType$>::SharedPtr m_tickService;/*END_TICK_CMD*/
 	/*HALT_RESPONSE*/std::atomic<bool> m_haltResult{false};/*END_HALT_RESPONSE*/
-	/*HALT_CMD*/rclcpp::Service<bt_interfaces::srv::HaltAction>::SharedPtr m_haltService;/*END_HALT_CMD*/
+	/*HALT_CMD*/rclcpp::Service<bt_interfaces_dummy::srv::HaltAction>::SharedPtr m_haltService;/*END_HALT_CMD*/
 	/*DATAMODEL*/$skillName$SkillDataModel m_dataModel; /*END_DATAMODEL*/
 	/*TOPIC_SUBSCRIPTIONS_LIST_H*/
 	/*TOPIC_SUBSCRIPTION_H*/
-	rclcpp::Subscription<$eventData.interfaceData[interfaceDataType]$>::SharedPtr m_subscription_$eventData.functionName$;/*END_TOPIC_SUBSCRIPTION_H*/
+	rclcpp::Subscription<$eventData.interfaceName$::msg::$eventData.messageNameSnakeCase$>::SharedPtr m_subscription_$eventData.functionName$;/*END_TOPIC_SUBSCRIPTION_H*/
 	/*ACTION_LIST_H*//*ACTION_H*/
 	std::shared_ptr<rclcpp::Node> m_node_action;
 	std::mutex m_actionMutex;
